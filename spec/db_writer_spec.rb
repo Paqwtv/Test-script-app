2ff92708eb50fc7dd6f77528f3e20a0e3ef0dd0c
Dima Hurum <paqwtv@gmail.com>
2017-11-28 14:24:49 +0200

    Delete db_writer_spec.rb

--- spec/db_writer_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-writer = DbWriter.new
-
-context 'Encapsulation check' do
-  it 'should cause an error' do
-    expect { writer.prepared_dat }.to raise_error(NoMethodError)
-  end
-end
